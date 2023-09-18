051c13a51b8c09196f50ccbe550210349d9968d9
parisa <parifyzi@gmail.com>
2023-09-19 00:47:14 +0330

    show selected parent id

--- src/pages/posts/Create.jsx
+++ src/pages/posts/Create.jsx
@@ -218,7 +218,7 @@ export default function PostCreatePage() {
 
           <div className="flex-1 mt-4">
             <Select
-              value={selectedParent}
+              defaultValue={selectedParent}
               onChange={(selectedOption) => setSelectedParent(selectedOption)}
               options={parentOptions}
               className="basic-select"
