b1876debfe59cb5729e916c16c565e0f6a5d47bc
Faraz <ifarazir@gmail.com>
2023-12-21 10:06:47 +0330

    add tag delete option

--- src/api/tag.js
+++ src/api/tag.js
@@ -1,248 +1,247 @@
 import axios from "axios";
 
 axios.interceptors.response.use(response => {
-    return response;
+  return response;
 }, error => {
-    if (error.response.status === 401) {
-        //place your reentry code
-    }
-    return error;
+  if (error.response.status === 401) {
+    //place your reentry code
+  }
+  return error;
 });
 
 const youzAxios = axios.create({
-    baseURL: import.meta.env.VITE_API_BASE,
-    headers: {
-        'Accept': 'application/json',
-    }
+  baseURL: import.meta.env.VITE_API_BASE,
+  headers: {
+    'Accept': 'application/json',
+  }
 });
 
 export const getTagsApi = async (authHeader) => {
-    return youzAxios.get('/api/admin/tags', {
-        headers: {
-            Authorization: authHeader
-        }
-    }).then((response) => {
-        if (response.data.status == 'success') {
-            return {
-                status: 'success',
-                variant: 'default',
-                message: response.data.message,
-                response: response?.data
-            }
-        }
-        else {
-            return {
-                status: 'error',
-                variant: 'error',
-                message: response?.data?.message,
-                response: response
-            }
-        }
-    }).catch((response) => {
-        return {
-            status: 'error',
-            variant: 'error',
-            message: response?.response?.data?.message,
-            response: response?.response?.data
-        }
-    })
+  return youzAxios.get('/api/admin/tags', {
+    headers: {
+      Authorization: authHeader
+    }
+  }).then((response) => {
+    if (response.data.status == 'success') {
+      return {
+        status: 'success',
+        variant: 'default',
+        message: response.data.message,
+        response: response?.data
+      }
+    }
+    else {
+      return {
+        status: 'error',
+        variant: 'error',
+        message: response?.data?.message,
+        response: response
+      }
+    }
+  }).catch((response) => {
+    return {
+      status: 'error',
+      variant: 'error',
+      message: response?.response?.data?.message,
+      response: response?.response?.data
+    }
+  })
 };
 
-  export const createTagApi = async (authHeader, request) => {
-        return youzAxios.post('/api/admin/tags', request, {
-          headers: {
-              Authorization: authHeader
-          }
-      }).then((response) => {
-          if (response.data.status == 'success') {
-              return {
-                  status: 'success',
-                  variant: 'default',
-                  message: response.data.message,
-                  response: response?.data
-              }
-          }
-          else {
-              return {
-                  status: 'error',
-                  variant: 'error',
-                  message: response?.data?.message,
-                  response: response
-              }
-          }
-      }).catch((response) => {
-          return {
-              status: 'error',
-              variant: 'error',
-              message: response?.response?.data?.message,
-              response: response?.response?.data
-          }
-      })
+export const createTagApi = async (authHeader, request) => {
+  return youzAxios.post('/api/admin/tags', request, {
+    headers: {
+      Authorization: authHeader
     }
-  
-  export const updateTagApi = async (authHeader, tagId, updatedData) => {
-    try {
-      const response = await youzAxios.put(`/api/admin/tags/${tagId}`, updatedData, {
-        headers: {
-          Authorization: authHeader
-        }
-      });
-  
-      if (response.data.status === 'success') {
-        return {
-          status: 'success',
-          variant: 'default',
-          message: response.data.message,
-          response: response.data
-        };
-      } else {
-        return {
-          status: 'error',
-          variant: 'error',
-          message: response.data.message,
-          response: response.data
-        };
+  }).then((response) => {
+    if (response.data.status == 'success') {
+      return {
+        status: 'success',
+        variant: 'default',
+        message: response.data.message,
+        response: response?.data
       }
-    } catch (error) {
+    }
+    else {
       return {
         status: 'error',
         variant: 'error',
-        message: error.response?.data?.message || 'An error occurred while updating the tag.',
-        response: error.response
-      };
-    }
-  };
-  
-  export const deleteTagApi = async (authHeader, tagId) => {
-    try {
-      const response = await youzAxios.delete(`/api/admin/tags/${tagId}`, {
-        headers: {
-          Authorization: authHeader
-        }
-      });
-  
-      if (response.data.status === 'success') {
-        return {
-          status: 'success',
-          variant: 'default',
-          message: response.data.message,
-          response: response.data
-        };
-      } else {
-        return {
-          status: 'error',
-          variant: 'error',
-          message: response.data.message,
-          response: response.data
-        };
+        message: response?.data?.message,
+        response: response
       }
-    } catch (error) {
+    }
+  }).catch((response) => {
+    return {
+      status: 'error',
+      variant: 'error',
+      message: response?.response?.data?.message,
+      response: response?.response?.data
+    }
+  })
+}
+
+export const updateTagApi = async (authHeader, tagId, updatedData) => {
+  try {
+    const response = await youzAxios.put(`/api/admin/tags/${tagId}`, updatedData, {
+      headers: {
+        Authorization: authHeader
+      }
+    });
+
+    if (response.data.status === 'success') {
+      return {
+        status: 'success',
+        variant: 'default',
+        message: response.data.message,
+        response: response.data
+      };
+    } else {
       return {
         status: 'error',
         variant: 'error',
-        message: error.response?.data?.message || 'An error occurred while deleting the tag.',
-        response: error.response
+        message: response.data.message,
+        response: response.data
       };
     }
-  };
-  
-  export const getTagByIdApi = async (authHeader, tagId) => {
-    try {
-      const response = await youzAxios.get(`/api/admin/tags/${tagId}`, {
-        headers: {
-          Authorization: authHeader
-        }
-      });
-  
-      if (response.data.status === 'success') {
-        return {
-          status: 'success',
-          variant: 'default',
-          message: response.data.message,
-          response: response.data
-        };
-      } else {
-        return {
-          status: 'error',
-          variant: 'error',
-          message: response.data.message,
-          response: response.data
-        };
+  } catch (error) {
+    return {
+      status: 'error',
+      variant: 'error',
+      message: error.response?.data?.message || 'An error occurred while updating the tag.',
+      response: error.response
+    };
+  }
+};
+
+export const deleteTagApi = async (authHeader, tagId) => {
+  try {
+    const response = await youzAxios.delete(`/api/admin/tags/${tagId}`, {
+      headers: {
+        Authorization: authHeader
       }
-    } catch (error) {
+    });
+
+    if (response.data.status === 'success') {
+      return {
+        status: 'success',
+        variant: 'default',
+        message: response.data.message,
+        response: response.data
+      };
+    } else {
       return {
         status: 'error',
         variant: 'error',
-        message: error.response?.data?.message || 'An error occurred while retrieving the tag.',
-        response: error.response
+        message: response.data.message,
+        response: response.data
       };
     }
-  };
-  
-  export const getUserTags = async (authHeader) => {
-    try {
-      const response = await youzAxios.get('/api/v1/tags', {
-        headers: {
-          Authorization: authHeader
-        }
-      });
-  
-      if (response.data.status === 'success') {
-        return {
-          status: 'success',
-          variant: 'default',
-          message: response.data.message,
-          response: response.data
-        };
-      } else {
-        return {
-          status: 'error',
-          variant: 'error',
-          message: response.data.message,
-          response: response.data
-        };
+  } catch (error) {
+    return {
+      status: 'error',
+      variant: 'error',
+      message: error.response?.data?.message || 'An error occurred while deleting the tag.',
+      response: error.response
+    };
+  }
+};
+
+export const getTagByIdApi = async (authHeader, tagId) => {
+  try {
+    const response = await youzAxios.get(`/api/admin/tags/${tagId}`, {
+      headers: {
+        Authorization: authHeader
       }
-    } catch (error) {
+    });
+
+    if (response.data.status === 'success') {
+      return {
+        status: 'success',
+        variant: 'default',
+        message: response.data.message,
+        response: response.data
+      };
+    } else {
       return {
         status: 'error',
         variant: 'error',
-        message: error.response?.data?.message || 'An error occurred while retrieving tags.',
-        response: error.response
+        message: response.data.message,
+        response: response.data
       };
     }
-  };
+  } catch (error) {
+    return {
+      status: 'error',
+      variant: 'error',
+      message: error.response?.data?.message || 'An error occurred while retrieving the tag.',
+      response: error.response
+    };
+  }
+};
+
+export const getUserTags = async (authHeader) => {
+  try {
+    const response = await youzAxios.get('/api/v1/tags', {
+      headers: {
+        Authorization: authHeader
+      }
+    });
+
+    if (response.data.status === 'success') {
+      return {
+        status: 'success',
+        variant: 'default',
+        message: response.data.message,
+        response: response.data
+      };
+    } else {
+      return {
+        status: 'error',
+        variant: 'error',
+        message: response.data.message,
+        response: response.data
+      };
+    }
+  } catch (error) {
+    return {
+      status: 'error',
+      variant: 'error',
+      message: error.response?.data?.message || 'An error occurred while retrieving tags.',
+      response: error.response
+    };
+  }
+};
 
 
-  export const getUserTagByIdApi = async (authHeader, tagId) => {
-    try {
-      const response = await youzAxios.get(`/api/v1/tags/${tagId}`, {
-        headers: {
-          Authorization: authHeader
-        }
-      });
-  
-      if (response.data.status === 'success') {
-        return {
-          status: 'success',
-          variant: 'default',
-          message: response.data.message,
-          response: response.data
-        };
-      } else {
-        return {
-          status: 'error',
-          variant: 'error',
-          message: response.data.message,
-          response: response.data
-        };
+export const getUserTagByIdApi = async (authHeader, tagId) => {
+  try {
+    const response = await youzAxios.get(`/api/v1/tags/${tagId}`, {
+      headers: {
+        Authorization: authHeader
       }
-    } catch (error) {
+    });
+
+    if (response.data.status === 'success') {
+      return {
+        status: 'success',
+        variant: 'default',
+        message: response.data.message,
+        response: response.data
+      };
+    } else {
       return {
         status: 'error',
         variant: 'error',
-        message: error.response?.data?.message || 'An error occurred while retrieving the tag.',
-        response: error.response
+        message: response.data.message,
+        response: response.data
       };
     }
-  };
-  
+  } catch (error) {
+    return {
+      status: 'error',
+      variant: 'error',
+      message: error.response?.data?.message || 'An error occurred while retrieving the tag.',
+      response: error.response
+    };
+  }
+};
--- src/pages/tags/Index.jsx
+++ src/pages/tags/Index.jsx
@@ -5,7 +5,7 @@ import { useForm } from "react-hook-form";
 import { ToastContainer, toast } from "react-toastify";
 import Input from "../../utils/Input";
 import { useAuthHeader } from "react-auth-kit";
-import { createTagApi, getTagByIdApi, getTagsApi, updateTagApi } from "../../api/tag";
+import { createTagApi, deleteTagApi, getTagByIdApi, getTagsApi, updateTagApi } from "../../api/tag";
 import { Link } from "react-router-dom";
 import { CgMathPlus } from "react-icons/cg";
 import { useTheme } from "../../context/ThemeContext";
@@ -13,6 +13,9 @@ import { useTheme } from "../../context/ThemeContext";
 export default function TagIndexPage() {
   const authHeader = useAuthHeader();
   const [tags, setTags] = useState([]);
+
+  const [showConfirmation, setShowConfirmation] = useState(false);
+  const [tagIdToDelete, setTagIdToDelete] = useState(null);
 
   useEffect(() => {
     fetchPostsData();
@@ -31,6 +34,29 @@ export default function TagIndexPage() {
     } catch (error) {
       console.error("Error fetching post data:", error);
     }
+  };
+
+  const handleConfirmDelete = async () => {
+    if (tagIdToDelete) {
+      const response = await deleteTagApi(authHeader(), tagIdToDelete);
+      if (response.status === "success") {
+        setTags((prevTags) => prevTags.filter((tag) => tag.id !== tagIdToDelete));
+        toast.success(response.message);
+      } else {
+        console.error("Error deleting post:", response);
+        toast.error(response.message);
+      }
+    }
+    setShowConfirmation(false);
+  };
+
+  const handleDeleteClick = (tagId) => {
+    setTagIdToDelete(tagId);
+    setShowConfirmation(true);
+  };
+
+  const handleCancelDelete = () => {
+    setShowConfirmation(false);
   };
 
   return (
@@ -101,11 +127,52 @@ export default function TagIndexPage() {
                               Edit <span className="sr-only">, {tag.name}</span>
                             </Link>
 
+                            <button
+                              type="button"
+                              onClick={() => handleDeleteClick(tag.id)}
+                              className="rounded-full bg-red-50 px-4 py-1 text-sm font-medium text-red-600 shadow-sm hover:bg-red-100"
+                            >
+                              Delete <span className="sr-only">, {tag.name}</span>
+                            </button>
                           </td>
                         </tr>
                       ))}
                     </tbody>
                   </table>
+
+                  {showConfirmation && (
+                    <div className="fixed z-10 inset-0 overflow-y-auto">
+                      <div className="flex items-end justify-center min-h-screen pt-4 px-4 pb-20 text-center sm:block sm:p-0">
+                        <div className="fixed inset-0 transition-opacity">
+                          <div className={`absolute inset-0 opacity-75 bg-gray-900 dark:bg-gray-900`}></div>
+                        </div>
+                        <span className="hidden sm:inline-block sm:align-middle sm:h-screen"></span>&#8203;
+                        <div className="inline-block align-bottom bg-gray-800 rounded-lg text-left overflow-hidden shadow-xl transform transition-all sm:my-8 sm:align-middle sm:max-w-lg sm:w-full">
+                          <div className={`px-4 pt-5 pb-4 sm:p-6 sm:pb-4 bg-gray-100 dark:bg-gray-900`}>
+                            <h3 className="text-lg leading-6 font-medium">Confirm Deletion</h3>
+                            <div className="mt-2">
+                              <p className="text-sm text-gray-400">Are you sure you want to delete this item?</p>
+                            </div>
+                          </div>
+                          <div className={`px-4 py-3 sm:px-6 sm:flex sm:flex-row-reverse bg-gray-200 dark:bg-gray-800`}>
+                            <button
+                              onClick={handleConfirmDelete}
+                              className="w-full inline-flex justify-center rounded-md border border-transparent shadow-sm px-4 py-2 bg-red-600 text-base font-medium text-white hover:bg-red-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-red-500 sm:ml-3 sm:w-auto sm:text-sm"
+                            >
+                              Delete
+                            </button>
+                            <button
+                              onClick={handleCancelDelete}
+                              className={`mt-3 w-full inline-flex justify-center rounded-md px-4 py-2 text-base font-medium text-gray-700 hover:bg-gray-300 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-gray-400 sm:mt-0 sm:w-auto sm:text-sm`}
+                            >
+                              Cancel
+                            </button>
+                          </div>
+                        </div>
+                      </div>
+                    </div>
+                  )}
+
                 </div>
               </div>
             </div>
